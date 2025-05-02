970cc90adc4276402f4e921e510b58b2296ed44b
qcloop <umedzokirov@hotmail.com>
2025-05-02 13:14:08 -0700

    Weight Delta from Target

--- gpt/gpt/main.py
+++ gpt/gpt/main.py
@@ -5,6 +5,7 @@ from tokenizer.encoder import Encoder
 import torch
 from torch import nn
 import matplotlib.pyplot as plt
+import tiktoken
 
 GPT_CONFIG_124M = {
     "vocab_size": 50257,  # Vocabulary size
@@ -132,12 +133,65 @@ def __tensor_fun():
     print(z.shape)
     # Expected output: torch.Size([2, 2, 1, 2]) 
 
+
+def ___probas_target():
+    GPT_CONFIG_124M = {
+        "vocab_size": 50257,  # Vocabulary size
+        "context_length": 1024,  # Context length
+        "emb_dim": 768,  # Embedding dimension
+        "n_heads": 12,  # Number of attention heads
+        "n_layers": 12,  # Number of layers
+        "drop_rate": 0.1,  # Dropout rate
+        "qkv_bias": False,  # Query-Key-Value bias
+    }
+    generator = TextGenerator(config=GPT_CONFIG_124M)
+    
+    inputs = torch.tensor([[16833, 3626, 6100],  
+                       [40,    1107, 588]])  
+    
+    targets = torch.tensor([[3626, 6100, 345  ],  
+                        [1107,  588, 11311]]) 
+    
+    start_context = "Every effort moves you"
+    tokenizer = tiktoken.get_encoding("gpt2")
+    
+    
+    with torch.no_grad():
+        logits = generator.model(inputs)
+        
+    probas = torch.softmax(logits, dim=-1) # Probability of each token in vocabulary
+    print(probas.shape) # Shape: (batch_size, num_tokens, vocab_size
+    
+    token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+    print("Token IDs:\n", token_ids)
+    
+    text_idx = 1
+    print(f"Targets batch 1: {token_ids_to_text(targets[text_idx], tokenizer)}")
+    print(f"Outputs batch 1: {token_ids_to_text(token_ids[text_idx].flatten(), tokenizer)}")
+    
+    #text_idx = 1
+    print(f"targets[text_idx]: {targets[text_idx]}")
+    target_probas_1 = probas[text_idx, [0,1,2], [ 1107,   588, 11311]]
+    print("Text 1:", target_probas_1)
+    
+    
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0) # add batch dimension
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0) # remove batch dimension
+    return tokenizer.decode(flat.tolist())
+
 def main():
-  result = __text_generation("Here we go Panthers", 10)
-  print(result)
-  my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  print(my_array[-20:])
-  print(my_array[:-20])
+  #result = __text_generation("Here we go Panthers", 10)
+  #print(result)
+  #my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  #print(my_array[-20:])
+  #print(my_array[:-20])
+  
+  ___probas_target()
  
 
 
