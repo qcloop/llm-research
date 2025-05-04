e5e3c26833624966b75b5a3201d0d5c09ca26a57
qcloop <umedzokirov@hotmail.com>
2025-05-03 19:46:27 -0700

    WIP
    Load weights, assign to a new model instance and perform basic generation(aka completion)

--- gpt/gpt/main.py
+++ gpt/gpt/main.py
@@ -1,4 +1,9 @@
-from open_ai.download_weights import download_gpt2_files
+from models.gpt_v2 import GPTV2
+from open_ai.pretrained_weights import (
+    download_gpt2_files,
+    load_params_from_file,
+    load_weights_into_gpt,
+)
 from text_generator import TextGenerator
 from models.dummy import DummyGPTModel
 from activations.gelu import GELU
@@ -132,7 +137,7 @@ def __tensor_fun():
     z = torch.squeeze(x, 3)
     print(z)
     print(z.shape)
-    # Expected output: torch.Size([2, 2, 1, 2]) 
+    # Expected output: torch.Size([2, 2, 1, 2])
 
 
 def ___probas_target():
@@ -146,38 +151,37 @@ def ___probas_target():
         "qkv_bias": False,  # Query-Key-Value bias
     }
     generator = TextGenerator(config=GPT_CONFIG_124M)
-    
-    inputs = torch.tensor([[16833, 3626, 6100],  
-                       [40,    1107, 588]])  
-    
-    targets = torch.tensor([[3626, 6100, 345  ],  
-                        [1107,  588, 11311]]) 
-    
+
+    inputs = torch.tensor([[16833, 3626, 6100], [40, 1107, 588]])
+
+    targets = torch.tensor([[3626, 6100, 345], [1107, 588, 11311]])
+
     start_context = "Every effort moves you"
     tokenizer = tiktoken.get_encoding("gpt2")
-    
-    
+
     with torch.no_grad():
         logits = generator.model(inputs)
-        
-    probas = torch.softmax(logits, dim=-1) # Probability of each token in vocabulary
-    print(probas.shape) # Shape: (batch_size, num_tokens, vocab_size
-    
+
+    probas = torch.softmax(logits, dim=-1)  # Probability of each token in vocabulary
+    print(probas.shape)  # Shape: (batch_size, num_tokens, vocab_size
+
     token_ids = torch.argmax(probas, dim=-1, keepdim=True)
     print("Token IDs:\n", token_ids)
-    
+
     text_idx = 1
     print(f"Targets batch 1: {token_ids_to_text(targets[text_idx], tokenizer)}")
-    print(f"Outputs batch 1: {token_ids_to_text(token_ids[text_idx].flatten(), tokenizer)}")
-    
+    print(
+        f"Outputs batch 1: {token_ids_to_text(token_ids[text_idx].flatten(), tokenizer)}"
+    )
+
     text_idx = 0
     print(f"targets[text_idx]: {targets[text_idx]}")
-    target_probas_1 = probas[text_idx, [0,1,2], targets[text_idx]]
+    target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
     print("Text 1:", target_probas_1)
 
     text_idx = 1
     print(f"targets[text_idx]: {targets[text_idx]}")
-    target_probas_2 = probas[text_idx, [0,1,2], targets[text_idx]]
+    target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
     print("Text 2:", target_probas_2)
 
     log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
@@ -185,8 +189,8 @@ def ___probas_target():
     avg_log_probas = torch.mean(log_probas)
     print("Log probas:", avg_log_probas * -1)
 
-    #using cross entropy, should produce same result
-    logits_flat = logits.flatten(0,1)
+    # using cross entropy, should produce same result
+    logits_flat = logits.flatten(0, 1)
     targets_flat = targets.flatten(0)
     print("logits_flats:", logits_flat.shape)
     print("targets_flat:", targets_flat.shape)
@@ -195,28 +199,31 @@ def ___probas_target():
     perpelexity = torch.exp(avg_log_probas * -1)
     print("perpelexity:", perpelexity)
 
-    
-    
+
 def text_to_token_ids(text, tokenizer):
-    encoded = tokenizer.encode(text, allowed_special={'<|endoftext|>'})
-    encoded_tensor = torch.tensor(encoded).unsqueeze(0) # add batch dimension
+    encoded = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)  # add batch dimension
     return encoded_tensor
 
+
 def token_ids_to_text(token_ids, tokenizer):
-    flat = token_ids.squeeze(0) # remove batch dimension
+    flat = token_ids.squeeze(0)  # remove batch dimension
     return tokenizer.decode(flat.tolist())
 
 
-pre_trained_model_dir ="/Users/uzokirov/Development/Python/llm-research/pre-trained_weights"
+pre_trained_model_dir = (
+    "/Users/uzokirov/Development/Python/llm-research/pre-trained_weights/124M"
+)
+
 
 def download_pretrained_weights():
     CHOOSE_MODEL = "gpt2-small (124M)"
 
     BASE_CONFIG = {
-        "vocab_size": 50257,     # Vocabulary size
+        "vocab_size": 50257,  # Vocabulary size
         "context_length": 1024,  # Context length
-        "drop_rate": 0.0,        # Dropout rate
-        "qkv_bias": True         # Query-key-value bias
+        "drop_rate": 0.0,  # Dropout rate
+        "qkv_bias": True,  # Query-key-value bias
     }
 
     model_configs = {
@@ -230,16 +237,87 @@ def download_pretrained_weights():
     model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
     download_gpt2_files(model_size, pre_trained_model_dir)
 
+
+def load_model_params():
+    settings, params = load_params_from_file(pre_trained_model_dir, "hparams.json")
+    print("Settings: ", settings)
+    print("Params dictionary keys: ", params.keys())
+    print(params["wte"])
+    print("Token embedding weight tensor dimensions: ", params["wte"].shape)
+
+def use_pretrained_weights():
+    # Define model configurations in a dictionary for compactness
+    model_configs = {
+        "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+        "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+        "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+        "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+    }
+
+    # Copy the base configuration and update with specific model settings
+    model_name = "gpt2-small (124M)"  # Example model name
+    NEW_CONFIG = GPT_CONFIG_124M.copy()
+    NEW_CONFIG.update(model_configs[model_name])
+    NEW_CONFIG.update({"context_length": 1024, "qkv_bias": True})
+
+    gpt = GPTV2(NEW_CONFIG)
+    gpt.eval()
+
+    settings, params = load_params_from_file(pre_trained_model_dir, "hparams.json")
+    load_weights_into_gpt(gpt, params)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+    gpt.to(device)
+    torch.manual_seed(123)
+    start_context = "Every effort moves you"
+    tokenizer = tiktoken.get_encoding("gpt2")
+
+    token_ids = generate_text_simple(
+        model=gpt,
+        idx=text_to_token_ids(start_context, tokenizer),
+        max_new_tokens=35,
+        context_size=GPT_CONFIG_124M["context_length"]
+    )
+
+    print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    # idx is (B, T) array of indices in the current context
+    for _ in range(max_new_tokens):
+
+        # Crop current context if it exceeds the supported context size
+        # E.g., if LLM supports only 5 tokens, and the context size is 10
+        # then only the last 5 tokens are used as context
+        idx_cond = idx[:, -context_size:]
+
+        # Get the predictions
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        # Focus only on the last time step
+        # (batch, n_token, vocab_size) becomes (batch, vocab_size)
+        logits = logits[:, -1, :]
+
+        # Get the idx of the vocab entry with the highest logits value
+        idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch, 1)
+
+        # Append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
+
+    return idx
+
 def main():
-  #result = __text_generation("Here we go Panthers", 10)
-  #print(result)
-  #my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  #print(my_array[-20:])
-  #print(my_array[:-20])
-  
-  #___probas_target()
-  download_pretrained_weights()
- 
+    # result = __text_generation("Here we go Panthers", 10)
+    # print(result)
+    # my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # print(my_array[-20:])
+    # print(my_array[:-20])
+
+    # ___probas_target()
+    # download_pretrained_weights()
+    # load_model_params()
+    use_pretrained_weights()
 
 
 if __name__ == "__main__":
--- gpt/gpt/open_ai/pretrained_weights.py
+++ gpt/gpt/open_ai/pretrained_weights.py
@@ -11,8 +11,9 @@ import urllib.request
 import json
 import numpy as np
 import tensorflow as tf
+import torch
 from tqdm import tqdm
-
+import numpy as np
 
 def download_gpt2_files(model_size, models_dir):
     # Validate model size
@@ -25,9 +26,13 @@ def download_gpt2_files(model_size, models_dir):
     base_url = "https://openaipublic.blob.core.windows.net/gpt-2/models"
     backup_base_url = "https://f001.backblazeb2.com/file/LLMs-from-scratch/gpt2"
     filenames = [
-        "checkpoint", "encoder.json", "hparams.json",
-        "model.ckpt.data-00000-of-00001", "model.ckpt.index",
-        "model.ckpt.meta", "vocab.bpe"
+        "checkpoint",
+        "encoder.json",
+        "hparams.json",
+        "model.ckpt.data-00000-of-00001",
+        "model.ckpt.index",
+        "model.ckpt.meta",
+        "vocab.bpe",
     ]
 
     # Download files
@@ -39,13 +44,16 @@ def download_gpt2_files(model_size, models_dir):
         download_file(file_url, file_path, backup_url)
 
 
-def load_params_from_file(model_dir:str, file_name:str):
-     # Load settings and params
+def load_params_from_file(model_dir: str, file_name: str):
+    # Load settings and params
     tf_ckpt_path = tf.train.latest_checkpoint(model_dir)
-    settings = json.load(open(os.path.join(model_dir, file_name), "r", encoding="utf-8"))
+    settings = json.load(
+        open(os.path.join(model_dir, file_name), "r", encoding="utf-8")
+    )
     params = load_gpt2_params_from_tf_ckpt(tf_ckpt_path, settings)
 
     return settings, params
+
 
 def download_file(url, destination, backup_url=None):
     def _attempt_download(download_url):
@@ -64,7 +72,12 @@ def download_file(url, destination, backup_url=None):
 
             # Initialize the progress bar with total file size
             progress_bar_description = os.path.basename(download_url)
-            with tqdm(total=file_size, unit="iB", unit_scale=True, desc=progress_bar_description) as progress_bar:
+            with tqdm(
+                total=file_size,
+                unit="iB",
+                unit_scale=True,
+                desc=progress_bar_description,
+            ) as progress_bar:
                 with open(destination, "wb") as file:
                     while True:
                         chunk = response.read(block_size)
@@ -156,3 +169,70 @@ def load_gpt2_params_from_tf_ckpt(ckpt_path, settings):
         target_dict[last_key] = variable_array
 
     return params
+
+def assign(left, right):
+    if left.shape != right.shape:
+        raise ValueError(f"Shape mismatch. Left: {left.shape}, Right: {right.shape}")
+    return torch.nn.Parameter(torch.tensor(right))
+
+
+def load_weights_into_gpt(gpt, params):
+    gpt.pos_emb.weight = assign(gpt.pos_emb.weight, params['wpe'])
+    gpt.tok_emb.weight = assign(gpt.tok_emb.weight, params['wte'])
+    
+    for b in range(len(params["blocks"])):
+        q_w, k_w, v_w = np.split(
+            (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.weight = assign(
+            gpt.trf_blocks[b].att.W_query.weight, q_w.T)
+        gpt.trf_blocks[b].att.W_key.weight = assign(
+            gpt.trf_blocks[b].att.W_key.weight, k_w.T)
+        gpt.trf_blocks[b].att.W_value.weight = assign(
+            gpt.trf_blocks[b].att.W_value.weight, v_w.T)
+
+        q_b, k_b, v_b = np.split(
+            (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.bias = assign(
+            gpt.trf_blocks[b].att.W_query.bias, q_b)
+        gpt.trf_blocks[b].att.W_key.bias = assign(
+            gpt.trf_blocks[b].att.W_key.bias, k_b)
+        gpt.trf_blocks[b].att.W_value.bias = assign(
+            gpt.trf_blocks[b].att.W_value.bias, v_b)
+
+        gpt.trf_blocks[b].att.out_proj.weight = assign(
+            gpt.trf_blocks[b].att.out_proj.weight, 
+            params["blocks"][b]["attn"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].att.out_proj.bias = assign(
+            gpt.trf_blocks[b].att.out_proj.bias, 
+            params["blocks"][b]["attn"]["c_proj"]["b"])
+
+        gpt.trf_blocks[b].ff.layers[0].weight = assign(
+            gpt.trf_blocks[b].ff.layers[0].weight, 
+            params["blocks"][b]["mlp"]["c_fc"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[0].bias = assign(
+            gpt.trf_blocks[b].ff.layers[0].bias, 
+            params["blocks"][b]["mlp"]["c_fc"]["b"])
+        gpt.trf_blocks[b].ff.layers[2].weight = assign(
+            gpt.trf_blocks[b].ff.layers[2].weight, 
+            params["blocks"][b]["mlp"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[2].bias = assign(
+            gpt.trf_blocks[b].ff.layers[2].bias, 
+            params["blocks"][b]["mlp"]["c_proj"]["b"])
+
+        gpt.trf_blocks[b].norm1.scale = assign(
+            gpt.trf_blocks[b].norm1.scale, 
+            params["blocks"][b]["ln_1"]["g"])
+        gpt.trf_blocks[b].norm1.shift = assign(
+            gpt.trf_blocks[b].norm1.shift, 
+            params["blocks"][b]["ln_1"]["b"])
+        gpt.trf_blocks[b].norm2.scale = assign(
+            gpt.trf_blocks[b].norm2.scale, 
+            params["blocks"][b]["ln_2"]["g"])
+        gpt.trf_blocks[b].norm2.shift = assign(
+            gpt.trf_blocks[b].norm2.shift, 
+            params["blocks"][b]["ln_2"]["b"])
+
+    gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
+    gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+    gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
+    
